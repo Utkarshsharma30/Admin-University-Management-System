26299d92318e33792a8ea84d2815ac54dee421fc
Utkarsh Sharma <uditsharma1508@gmail.com>
2024-08-01 18:05:52 +0530

    commit1

---
+++ src/university/management/system/conn.java
@@ -0,0 +1,20 @@
+package university.management.system;
+
+import java.sql.*;
+
+public class conn {
+    
+    Connection c;
+    Statement s;
+
+    conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///ums", "root", "");
+            s = c.createStatement();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
